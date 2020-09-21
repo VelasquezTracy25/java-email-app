3ac50982fad9ae6bba79dbfe7bd5c0eff897fd30
Tracy Velasquez <velasqueztracy25@gmail.com>
2020-09-20 21:57:28 -0500

    added constructor for name

--- src/emailApp/EmailApp.java
+++ src/emailApp/EmailApp.java
@@ -1,4 +1,31 @@
 package emailApp;
 
+import java.util.Scanner;
+
 public class EmailApp {
+    public String firstName;
+    public String lastName;
+    public String password;
+    public String department;
+    public int mailboxCapacity;
+    public String alternateEmail;
+
+    //Constructor to receive the first name and last name
+    public EmailApp(String firstName, String lastName) {
+        //class level var above ("this") = local var (argument);
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    //Ask for the department
+
+    Scanner scanner = Scanner(System.in);
+
+    // Generate a random password
+
+    //Set mailbox capacity
+
+    //Set alternate email
+
+    //Change the password
 }
