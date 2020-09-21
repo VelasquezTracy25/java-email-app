db5881a24f33538dcf2151ba12fe549f6ba422ab
Tracy Velasquez <velasqueztracy25@gmail.com>
2020-09-20 22:19:46 -0500

    added set department menu, and added to the constructor for department

--- src/emailApp/Email.java
+++ src/emailApp/Email.java
@@ -1,4 +1,55 @@
 package emailApp;
 
+import java.util.Scanner;
+
 public class Email {
+
+    public String firstName;
+    public String lastName;
+    public String password;
+    public String department;
+    public int mailboxCapacity;
+    public String alternateEmail;
+
+    //Constructor to receive the first name and last name
+    public Email(String firstName, String lastName) {
+    //class level var above ("this") = local var (argument);
+        this.firstName = firstName;
+        this.lastName = lastName;
+        System.out.println("Email created: " + this.firstName + " " + this.lastName);
+
+    //call a method asking for dept - return dept
+        this.department = setDepartment();
+        System.out.println(department);
+    }
+
+    //Ask for the department
+
+    private String setDepartment() {
+        System.out.println("Enter the employee's department \n" +
+         "1 for Sale \n" +
+         "2 for Development \n" +
+         "3 for Account\n" +
+         "0 for none");
+        int input = new Scanner(System.in).nextInt();
+        switch (input) {
+            case 1:
+                return "sales";
+            case 2:
+                return "dev";
+            case 3:
+                return "accounting";
+            default:
+                return "";
+        }
+    }
+
+    // Generate a random password
+
+    //Set mailbox capacity
+
+    //Set alternate email
+
+    //Change the password
+
 }
--- src/emailApp/EmailApp.java
+++ src/emailApp/EmailApp.java
@@ -1,31 +1,8 @@
 package emailApp;
 
-import java.util.Scanner;
-
 public class EmailApp {
-    public String firstName;
-    public String lastName;
-    public String password;
-    public String department;
-    public int mailboxCapacity;
-    public String alternateEmail;
+    public static void main(String[] args) {
+        Email em1 = new Email("Tracy", "Velasquez");
 
-    //Constructor to receive the first name and last name
-    public EmailApp(String firstName, String lastName) {
-        //class level var above ("this") = local var (argument);
-        this.firstName = firstName;
-        this.lastName = lastName;
     }
-
-    //Ask for the department
-
-    Scanner scanner = Scanner(System.in);
-
-    // Generate a random password
-
-    //Set mailbox capacity
-
-    //Set alternate email
-
-    //Change the password
 }
